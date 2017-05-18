d8bee2754905972c6bcd608be7725a8f3c752ac9
Patrícia Guimarães <deud.patricia@yahoo.com.br>
2017-05-18 14:15:23 -0300

    Update and rename teste5 to teste5.asm
    ;erros: declaraço ausente de L1, declaraço repetida de L2, diretiva inválida EQUAL; dois rótulos (var e val) na mesma linha;
    ; mais de um argumento em uma instrução; não tem stop

--- testes/teste5.asm
+++ testes/teste5.asm
@@ -1,7 +1,7 @@
 ; Teste 5 
 
 ;erros: declaraço ausente de L1, declaraço repetida de L2, diretiva inválida EQUAL; dois rótulos (var e val) na mesma linha;
-; mais de um argumento em uma instrução
+; mais de um argumento em uma instrução; não tem stop
 
 
 L2: EQU 0
@@ -17,7 +17,7 @@ LOAD N+2
 var2: var1: load N ; 2 rts
 ADD val,n+3 ; mais de um arg
 JMP Var
-
+;falta stop
 SECTION DATA
 N: SPACE 3
 Var1: space
