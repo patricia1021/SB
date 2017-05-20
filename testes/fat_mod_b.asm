91437899f8f34c36006b3db7d9bdd374992cd0c3
Patrícia Guimarães <deud.patricia@yahoo.com.br>
2017-05-20 17:32:22 -0300

    Create fat_mod_b.asm

---
+++ testes/fat_mod_b.asm
@@ -0,0 +1,15 @@
+SECTION TEXT
+mod_b: begin
+b: extern
+h: extern
+public soma
+public mod_b
+load b
+mult h ;instrução errada
+div dois
+store area
+
+SECTION DATA
+dois: const 2
+area: space
+; sem end
