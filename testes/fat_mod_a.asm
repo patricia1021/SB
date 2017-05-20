a53a6ed0160cdd624e8f951217de4268f6fcd16b
Patrícia Guimarães <deud.patricia@yahoo.com.br>
2017-05-20 17:32:08 -0300

    Update and rename teste4.asm to fat_mod_a.asm

--- testes/fat_mod_a.asm
+++ testes/fat_mod_a.asm
@@ -20,18 +20,3 @@ h: space
 stop ; instrução no local errado
 end ; instrução no local errado
 
-SECTION TEXT
-mod_b: begin
-b: extern
-h: extern
-public soma
-public mod_b
-load b
-mult h ;instrução errada
-div dois
-store area
-
-SECTION DATA
-dois: const 2
-area: space
-; sem end
